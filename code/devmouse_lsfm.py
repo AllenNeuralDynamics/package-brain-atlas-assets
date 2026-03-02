52a0c90f5de0dbd380307fe4dd9cada2ceb73c47
dyf <david.feng@alleninstitute.org>
2026-03-02 16:23:03 +0000

    unbreaking devmouse lsfm

--- code/devmouse_lsfm.py
+++ code/devmouse_lsfm.py
@@ -217,8 +217,8 @@ def package_age_group(age: str, base_dir: Path, results_dir: Path, asset_library
         return
 
     # Define paths
-    template_dir = base_dir / f"templates/allen-dev-mouse-{age}-lsfm-template/2024"
-    annotation_dir = base_dir / f"annotation-sets/allen-dev-mouse-{age}-lsfm-annotation/2024"
+    template_dir = base_dir / f"templates/allen-dev-mouse-{age}-template/2024"
+    annotation_dir = base_dir / f"annotation-sets/allen-dev-mouse-{age}-annotation/2024"
 
     # Check if directories exist
     if not os.path.exists(template_dir):
@@ -241,7 +241,7 @@ def package_age_group(age: str, base_dir: Path, results_dir: Path, asset_library
     _write_devmouse_template_data_description(template.location(results_dir), age)
 
     # Create annotation set
-    annotation_name = f"allen-dev-mouse-{age.lower()}-annotation"
+    annotation_name = f"allen-dev-mouse-{age.lower()}-lsfm-annotation"
     annotation_set = AnnotationSet(
         name=annotation_name,
         template=template,
