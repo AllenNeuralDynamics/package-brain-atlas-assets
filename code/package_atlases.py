dcfb03696d082e02a9291a3336b523420c4ac389
dyf <david.feng@alleninstitute.org>
2026-03-04 13:27:06 +0000

    full build

--- code/package_atlases.py
+++ code/package_atlases.py
@@ -119,10 +119,7 @@ def main(results_dir: str | Path):
     ## Package DevMouse atlas assets first
     devmouse.package_devmouse(devmouse_dir, results_dir, library)
     devmouse_spim.package_devmouse(devmouse_lsfm_dir, results_dir, library)
-
-    # devmouse only build
-    return
-
+    
     # Package CCF 3 legacy annotations and templates
     CCFv3.package_ccf(ccf3_dir, results_dir, library, scales=scales)
 
